bb2a8356cf212d91c28e32267630003ac4efb34d
Andrey Marchenko <andrey.marchenko@datadoghq.com>
2025-02-18 11:58:01 +0100

    logging that shows that active trace is not picked up in the controller, why?

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,12 @@ class WelcomeController < ApplicationController
   layout false
 
   def index
+    p "here I start the controller action"
+    p "My PID is #{Process.pid}"
+    p "Active trace is #{Datadog::Tracing.active_trace.id}"
+    Datadog::Tracing.trace("welcome#index") do
+      p "boo"
+    end
+    p "here I end the controller action"
   end
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,11 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    p "here I start the welcome test"
+    p "My PID is #{Process.pid}"
+    p "Active trace is #{Datadog::Tracing.active_trace.id}"
     get root_url
     assert_response :success
+    p "here I end the test"
   end
 end
